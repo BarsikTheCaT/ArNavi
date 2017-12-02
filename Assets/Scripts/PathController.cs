8c7cecf5dc733d6cb0a4ec25646a499e17317521
Barsik_The_CaT <barsikdacat@gmail.com>
2017-12-02 17:40:46 +1000

    Initial commit

---
+++ Assets/Scripts/PathController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathController : MonoBehaviour
+{
+    public GameObject[] Paths;
+
+    private int currPath = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (var p in Paths) {
+            p.SetActive(false);
+        }
+    }
+
+    public void SetPath(int idx)
+    {
+        UIManager.instance.OpenBasePanel();
+        Paths[currPath].SetActive(false);
+        Paths[idx].SetActive(true);
+        NavController.status = "Show path";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
